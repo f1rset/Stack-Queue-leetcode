0d301a2fcc5771bf776900a5c4e28f6d1b53669d
f1rset <stekostant@gmail.com>
2024-04-16 12:15:52 +0300

    fixed bugs

--- freq_stack/freq_stack.py
+++ freq_stack/freq_stack.py
@@ -1,22 +1,23 @@
-from collections import deque
-class FreqStack:
+from collections import defaultdict, deque
 
+class FreqStack:
     def __init__(self):
         self.stack = deque()
+        self.freq_map = defaultdict(int)
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        self.freq_map[val] += 1
 
     def pop(self) -> int:
-        max_occ = max([self.stack.count(i) for i in self.stack])
-        elements = [i for i in self.stack if self.stack.count(i) == max_occ]
-        index = max([i for i, value in enumerate(self.stack) if value in elements])
-        el = self.stack[index]
-        del self.stack[index]
-        return el
-
-
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
+        max_freq = max(self.freq_map.values())
+        max_freq_elements = [key for key, value in self.freq_map.items() if value == max_freq]
+        # Find the most recently added element among those with max frequency
+        for i in range(len(self.stack) - 1, -1, -1):
+            if self.stack[i] in max_freq_elements:
+                el = self.stack[i]
+                del self.stack[i]
+                self.freq_map[el] -= 1
+                if self.freq_map[el] == 0:
+                    del self.freq_map[el]
+                return el
