d80e65a394b77b238b384c50fb6cdb6c9b7763d2
f1rset <stekostant@gmail.com>
2024-04-16 11:12:22 +0300

    redone from list to deque | everything done

--- freq_stack/freq_stack.py
+++ freq_stack/freq_stack.py
@@ -1,7 +1,8 @@
+from collections import deque
 class FreqStack:
 
     def __init__(self):
-        self.stack = []
+        self.stack = deque()
 
     def push(self, val: int) -> None:
         self.stack.append(val)
@@ -10,8 +11,9 @@ class FreqStack:
         max_occ = max([self.stack.count(i) for i in self.stack])
         elements = [i for i in self.stack if self.stack.count(i) == max_occ]
         index = max([i for i, value in enumerate(self.stack) if value in elements])
-
-        return self.stack.pop(index)
+        el = self.stack[index]
+        del self.stack[index]
+        return el
 
 
 # Your FreqStack object will be instantiated and called as such:
